8864ce9298c442547b4bd22e4d55aca22017d81c
KABOBLACK <159945531+KABOBLACK@users.noreply.github.com>
2026-08-08 23:09:24 +0200

    Update backtester.py

--- backtest/backtester.py
+++ backtest/backtester.py
@@ -1,5 +1,5 @@
-import pandas as pd
 from strategy.strategy import generate_signal
+from backtest.performance import calculate_performance
 
 
 def run_backtest(data, starting_balance=1000):
@@ -7,25 +7,36 @@ def run_backtest(data, starting_balance=1000):
     position = None
     entry_price = 0
 
+    trades = []
+
     for i in range(30, len(data)):
         current_data = data.iloc[:i + 1]
         price = data["close"].iloc[i]
 
         signal = generate_signal(current_data)
 
-        # Open position
+        # Open long position
         if signal == "BUY" and position is None:
             position = "LONG"
             entry_price = price
 
-        # Close position
+        # Close long position
         elif signal == "SELL" and position == "LONG":
             profit = price - entry_price
+
             balance += profit
+
+            trades.append({
+                "entry_price": entry_price,
+                "exit_price": price,
+                "profit": profit
+            })
+
             position = None
 
-    return {
-        "starting_balance": starting_balance,
-        "ending_balance": balance,
-        "profit": balance - starting_balance
-    }
+    performance = calculate_performance(
+        trades,
+        starting_balance
+    )
+
+    return performance
